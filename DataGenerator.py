5a0f5715d999b6bed766faf5a5a24183e4381ba7
Harutyun Hovhannisyan <hhovhannisya@vmware.com>
2019-02-05 16:42:06 +0400

    added simple prototype of generate timeseties class

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,3 +1,15 @@
+from DataType import DataType as dt
+
+
 class DataGenerator:
-    def __init__(self):
-        print("Data gen ctor")
+    def __init__(self, start, end, type, config):
+        self.__start = start
+        self.__end = end
+        self.__type = type
+        self.__config = config
+
+    def generate(self):
+        timestamps = [445465465, 54684564, 4864684]
+        values = [1, 2, 2]
+        print("generated . . . ")
+
