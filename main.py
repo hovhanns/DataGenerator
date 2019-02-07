7a3a4a5533ec53533914574b55891a77ad4dabd9
Harutyun Hovhannisyan <hhovhannisya@vmware.com>
2019-02-07 20:40:14 +0400

    added compleate POC version of generating timestamps

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-from generator import timeseries
 
-start = '2009/07/31'
-end = '2009/08/31'
-gen = timeseries.TimeSeriesGenerator(start, end, 'D', 10)
-print("starting . . ")
-for t in gen.next_point():
+from DataGenerator import DataGenerator
+from Config import Config
+
+conf = Config("Config/config.json").get_config()
+
+dgen = DataGenerator(conf)
+tplist = list(dgen.generate())
+print(len(tplist))
+for t in tplist:
     print(t)
