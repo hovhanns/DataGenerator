6b7198141665242a9862a4e3103c947fc95c1dac
Harutyun Hovhannisyan <hhovhannisya@vmware.com>
2019-02-07 20:38:53 +0400

    now we retrun zipped timestamps

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,15 +1,15 @@
-from DataType import DataType as dt
 from TimestampGenerator import TimestampGenerator
 from ValueGenerator import ValueGenerator
+
+
 class DataGenerator:
     def __init__(self, config):
         self.__config = config
 
     def generate(self):
-        tgen = TimestampGenerator(self.__config["start"],self.__config["stop"], self.__config["step"])
+        tgen = TimestampGenerator(self.__config["start"], self.__config["end"], self.__config["step"])
         timestamps = tgen.generate()
 
         vgen = ValueGenerator(self.__config["data"]["type"], len(timestamps))
         values = vgen.generate()
-        print("generated . . . ")
-
+        return zip(values, timestamps)
