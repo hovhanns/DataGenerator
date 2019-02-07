c05e84a02182fa9dc58f1fa112c701944e3e89b7
Harutyun Hovhannisyan <hhovhannisya@vmware.com>
2019-02-07 20:38:08 +0400

    added all value generating functions

--- ValueGenerator.py
+++ ValueGenerator.py
@@ -1,12 +1,26 @@
 """
     ValueGenerator generates values by given type and config
 """
+from generator import Core
+from DataType import DataType
 
 
 class ValueGenerator:
+    type_functions = {
+        DataType.CONSTANT: Core.const,
+        DataType.NEAR_CONSTANT: Core.near_constant,
+        DataType.TRENDY: Core.trendy,
+        DataType.NEAR_TRENDY: Core.near_trendy,
+        DataType.STATIONARY: Core.stationary,
+        DataType.TREND_STATIONARY: Core.trend_stationary,
+        DataType.UNIT_ROOT: "here should be unit root function"
+    }
+
     def __init__(self, type, size):
+        self.__type = type
+        self.__size = size
         print("generating values . . . ")
 
     def generate(self):
-        values = [1, 2, 3]
+        values = ValueGenerator.type_functions[DataType[self.__type]](size=self.__size)
         return values
---
+++ generator/Core.py
@@ -0,0 +1,61 @@
+"""
+this class created for core data generating functions
+"""
+import numpy as np
+
+
+class Core:
+    def __init__(self):
+        print("default ctor")
+
+    @staticmethod
+    def stationary(mean=20, scale=10, size=100):
+        """
+        mean:  center of a distribution
+        scale: width of the distribution
+        """
+        stat = np.random.normal(mean, scale, size)
+        stat = np.rint(stat)
+        return stat
+
+    @staticmethod
+    def trendy(k=3, b=3, size=100):
+        func = lambda x, k, b: (k * x + b)
+        linear = np.array([func(x, k, b) for x in range(1, size + 1)])
+        return linear
+
+    @staticmethod
+    def const(b=3, size=100):
+        return Core.trendy(0, b, size)
+
+    @staticmethod
+    def noise(noise_per=5, amplitude=10, size=100):
+        import time
+        np.random.seed(int(time.time()))
+        noise_per /= 100
+        noise_high = amplitude
+        noise_low = -1 * amplitude
+        randoms = np.random.randint(low=noise_low, high=noise_high, size=(int(size * noise_per)))
+        noise = np.array([0] * int(size * round(1 - noise_per, 2)))
+        noise = np.concatenate((noise, randoms))
+        if len(noise) != size:
+            noise = np.append(noise, np.random.randint(low=noise_low, high=noise_high))
+        np.random.shuffle(noise)
+        return noise
+
+    @staticmethod
+    def near_trendy(k=3, b=3, noise_per=5, amplitude=3, size=100):
+        tr = Core.trendy(k, b, size)
+        nc = Core.noise(noise_per=noise_per, size=size, amplitude=amplitude)
+        near_t = tr + nc
+        return near_t
+
+    @staticmethod
+    def near_constant(const=5, noise_per=5, amplitude=5, size=100):
+        return Core.near_trendy(k=0, b=const, noise_per=noise_per, amplitude=amplitude, size=size)
+
+    @staticmethod
+    def trend_stationary(k=3, b=3, mean=10, scale=10, size=100):
+        linear = Core.trendy(k=k, b=b, size=size)
+        stat = Core.stationary(mean=mean, scale=scale, size=size)
+        return linear + stat
