b51123c6064ac05d0e5576fdd8346a5a6f6b6b13
Harutyun Hovhannisyan <hhovhannisya@vmware.com>
2019-02-11 12:15:45 +0400

    merge conflict

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ from Config import Config
 
 # TODO here need to add Wavefront package to pip global library and install it as a requirement
 # sys.path.append("/Users/hhovhannisya/PycharmProjects/vROpsMonitoringViaWavefront")
-# from Wavefront import Process
+from Wavefront import Process
+from Wavefront import Importer
 
 conf = Config("Config/config.json").get_config()
 
-for metrics in conf:
-    for data in metrics["data"]:
+imp = Importer(conf["wavefront"]["cluster"], conf["wavefront"]["token"])
+for metric in conf["metrics"]:
+    for data in metric["data"]:
         dgen = DataGenerator(data)
         tplist = list(dgen.generate())
-        metrics_dict = {metrics["metric"]: tplist}
-        print(metrics_dict)
+        metrics_dict = {metric["metric"]: tplist}
+        processed = Process.process_data_dictionary(metrics_dict, metric["source"])
+        #print(processed)
+        print(imp.import_data(processed))
+        #print(metrics_dict)
 # dat = Process.process_historical_data_dictionary(metrics_dict, source_name="my_source", include_tag=False)
