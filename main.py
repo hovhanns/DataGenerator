d86bb6aab9f4db05507536bd081aee9c7c97df49
Harutyun Hovhannisyan <hhovhannisya@vmware.com>
2019-02-15 18:00:54 +0400

    added logging feature

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+import logging.config
+import yaml
 
 from DataGenerator import DataGenerator
 from Config import Config
 
-# TODO here need to add Wavefront package to pip global library and install it as a requirement
-# sys.path.append("/Users/hhovhannisya/PycharmProjects/vROpsMonitoringViaWavefront")
 from Wavefront import Process
 from Wavefront import Importer
+
+logging.config.dictConfig(yaml.load((open('logging.yaml', 'r'))))
+logger = logging.getLogger(__name__)
 
 conf = Config("Config/config.json").get_config()
 imp = Importer(conf["wavefront"]["cluster"], conf["wavefront"]["token"])
@@ -19,9 +22,9 @@ for metric in conf["metrics"]:
         metrics_dict = {metric["metric"]: tplist}
         processed = Process.process_data_dictionary(metrics_dict, metric["source"])
         #print(processed)
-        print(imp.import_data(processed))
+        logger.info(imp.import_data(processed))
         #print(metrics_dict)
 
-        print(metrics_dict)
+        logger.debug(metrics_dict)
 
 # dat = Process.process_historical_data_dictionary(metrics_dict, source_name="my_source", include_tag=False)
